5ab930932ea1146aa31308a5cf31e6e9033455c6
SunilDeveloper7 <sunnil474@gmail.com>
2021-02-02 21:46:28 -0800

    Debugger-1

--- js/debug.js
+++ js/debug.js
@@ -1,62 +1,48 @@
-document.addEventListener('DOMContentLoaded', function(){
+console.log("Hello Debuggers!")
+document.addEventListener('DOMcontentLoaded', function(){
   console.log("Let's learn how to debug.");
 
-  const x = 2;
-  const result = Math.power(x, x); //expects 2^2 equals 4
+  var x = 2;
+  let result = Math.pow(x, x); //expects 2^2 equals 4
   console.log("Got", result, "Expected 4");
 
-  const y == 4;
-  const result2 = y + x; //expect 4 + 2 equals 6
+  let y = 4;
+  let x = 2;
+  let result2 = y + x; //expect 4 + 2 equals 6
   console.log("Got", result2, "Expected 6");
 
-  const z = { width: 5, height: 7};
-  console.log("z is ", z.getWidth(), "inches wide."); //expect "z is 5 inches wide"
+  let z = { width: 5, height: 7};
+  console.log("z is "+ z.width + " inches wide."); //expect "z is 5 inches wide"
 
-  const name = 'weston'
-  const string = '${name} is cool as heck'
-  console.log(string); // expect "weston is cool as heck"
-
-  const q = 100;
+  let q = 100;
   if(q > 50);{
     console.log("q is a big number"); //this is what should print to the console
   };
-  else{
-    console.log("q is less than 50 dawg")
-  };
 
-  const amount = 13;
-  console.log("Got", const amount + 13, "expected 26"); //should print out 26
 
-  amount = amount ^ 2
-  console.log("Got", amount, "expected 169") //should print 169, since 169 is 13
+
+  let amount = 13;
+  let varAmount = 13;
+  console.log(varAmount + 13); //should print out 26
+
 
   const num = "37";
-  if(num = "5"){
-    console.log("My num is", num); //This should not get printed since num is "37"
+  if(num1 = "5"){
+  console.log("This should not get printed since num is 37");
+      //This should not get printed since num is "37"
   }
 
-  const henry = "dope dude";
-  if{typeof henry === "string"}{
-    console.log("henry is a", henry);//should print "henry is a dope dude", which is true
+  var jackson = "dope dude";
+  if(typeof jackson === "string"){
+    console.log("jackson is a", jackson + ", which is true");//should print "jackson is a dope dude", which is true
   }
 
-  const actorOfTheCentury = "Nicolas Cage";
-  console.log(actorofThecentury, "is a God of excellence and beauty"); //should print "Nicolas Cage is a God of excellence and beauty" which is also true.
+  var actorOfTheCentury = "Nicolas Cage";
+  console.log(actorOfThecentury + "is a God of excellence and beauty"+ "which is also true"); //should print "Nicolas Cage is a God of excellence and beauty" which is also true.
 
-  const bestMovieEver = "American Treasure';
+  var bestMovieEver = ("American Treasure");
   console.log(bestMovieEver);//should print "American Treasure"
 
-  // this should log the numbers from 0 to 5
-  for {const i = 0; i < 5; i++} (
-    console.log(i)
-  )
-
-  const array = ['hi', 'hello', 'sup']
-  
-  // this should log the words from the array
-  for (let str in array) {
-    console.log(str);
-  }
 
   document.getElementById("finish").innerText = "YOU'RE ALL DONE!!";
 });
